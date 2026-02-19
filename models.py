f7e1141323f06ffb47af029a6647877d40ce85e8
Rafael <rafaelkopp37@gmail.com>
2026-02-18 21:16:06 -0300

    App att

---
+++ models.py
@@ -0,0 +1,10 @@
+
+import json
+import os
+class Usuario:
+    def __init__(self, nome, senha, email):
+        self.nome = nome
+        self.senha = senha
+        self.email = email
+
+
---
+++ utils.py
@@ -0,0 +1,3 @@
+import os
+def limpar():
+    os.system("cls" if os.name == "nt" else "clear")
